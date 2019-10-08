fce0ea6d891e2cc8f61904a51502cf6a803d7c81
Aurélien Hugues <aurelien.hugues@supelec.fr>
2019-10-08 22:47:54 +0200

    Remove commands from database schema

---
+++ migrations/20191008204611-remove-commands.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable func-names */
+
+let dbm;
+let type;
+let seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  db.dropTable('Command', callback);
+};
+
+exports.down = function (db, callback) {
+  db.createTable('Command', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true,
+      notNull: true,
+    },
+    title: {
+      type: 'string',
+      notNull: true,
+    },
+    remarks: {
+      type: 'string',
+      notNull: true,
+      defaultValue: '',
+    },
+  }, (err) => {
+    if (err) return callback(err);
+    return callback();
+  });
+};
+
+exports._meta = {
+  version: 1,
+};
