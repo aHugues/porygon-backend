aca64f5beb20ac4a288ff716cad7573ea64afc2a
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2020-01-13 21:30:42 +0100

    Ref #15 Add Healthcheck route
    Add a healthcheck route to display API status

--- routes/index.controller.js
+++ routes/index.controller.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const HealthcheckService = require('../services/healthcheck.service');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/healthcheck', (req, res) => {
+  res.json(HealthcheckService.returnStatus());
 });
 
 module.exports = router;
